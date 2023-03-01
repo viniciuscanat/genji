d8e3de68fcabea10a60cdd8cf4a4c8391e96efee
viniciuscanat <114233669+viniciuscanat@users.noreply.github.com>
2023-03-01 11:12:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function myFunction()  {
+var nome = document.getElementById ("nome") . value;
+ var resposta = "seja Bem Vindo:"
+document.getElementById ("resposta") . innerHTML = resposta + nome;
+}
+
+function myFunction2()  {
+    var objetivo = document.getElementById ("objetivo") . value;
+     var resposta2 = "Interessante, Meu objetivo é alcançar a paz eliminando todos meus inimigos "
+    document.getElementById ("resposta2") . innerHTML = resposta2;
+    
+    }
+
+    function myFunction3()  {
+        var persona = document.getElementById ("persona") . value;
+         var resposta3 = "wow amo esse personagem, Mas o genji é muito melhor "
+        document.getElementById ("resposta3") . innerHTML = resposta3;
+        
+        }
